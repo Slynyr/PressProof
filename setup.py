845b08e0a4a232d245494546befebd2ae702f932
Slynyr <99285452+Slynyr@users.noreply.github.com>
2025-09-22 16:03:05 -0400

    Fixed incorrect syntax in setup.py
    - Added missing , in setup.py
    - Version bump

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ with open("README.md", "r") as f:
 
 setup(
     name="PressProof",
-    version="1.0", 
+    version="1.1", 
     packages=find_packages(),
     install_requires=["colorama", "openai", "beautifulsoup4", "requests"],
     entry_points={
         "console_scripts": [
             "pressproof = pressproof.__main__:mainEntryPoint"
         ]
-    }
+    }, 
+    long_description=description,
+    long_description_content_type="text/markdown"
 )
