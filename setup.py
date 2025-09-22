f9dbae778b5282c5a247ca5d53c4c6ef0777eda0
Slynyr <99285452+Slynyr@users.noreply.github.com>
2025-09-22 16:06:07 -0400

    Fixed image not showing up on PyPi
    - Fixed README image not showing up on PyPi
    - Version bump

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 with open("README.md", "r") as f: 
     description = f.read()
+    description.replace("assets/example.png", "https://github.com/Slynyr/PressProof/raw/main/assets/example.png")
 
 setup(
     name="PressProof",
-    version="1.1", 
+    version="1.1.2", 
     packages=find_packages(),
     install_requires=["colorama", "openai", "beautifulsoup4", "requests"],
     entry_points={
